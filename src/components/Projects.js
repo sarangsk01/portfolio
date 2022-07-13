4ccc651daa5ee680746732cb5f2c27bcfc98c3c4
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-07-13 19:02:35 +0530

    portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -30,55 +30,77 @@ const Projects = (props) => {
               <p className="info">Information about World Clock.</p>
               <a
                 className="got-app"
-                href="https://people-of-got-2.netlify.app/"
+                href="https://world-clock-timer.netlify.app/"
               >
                 Demo
               </a>
               <a
                 className="got-app"
-                href="https://github.com/sarangsk01/World-Clock"
+                href="https://github.com/sarangsk01/world-clock-1"
               >
                 Source
               </a>
             </li>
             <li className="pro-card">
-              <h1 className="head">Movie Watch List App</h1>{' '}
-              <p className="info">Information about Movie Watch List App.</p>
+              <h1 className="head">Todo App With API</h1>{' '}
+              <p className="info">Information about Todo App.</p>
               <a
                 className="got-app"
-                href="https://github.com/sarangsk01/Movie-watch-list"
+                href="https://sarangsk01.github.io/TA-JS-asynchronous-js-TJaaao/block-BJaakh/code/index.html"
+              >
+                Demo
+              </a>
+              <a
+                className="got-app"
+                href="https://github.com/sarangsk01/TA-JS-asynchronous-js-TJaaao/tree/block-BJaakh/block-BJaakh"
               >
                 Source
               </a>
             </li>
             <li className="pro-card">
-              <h1 className="head">Digital Marketing Website</h1>{' '}
-              <p className="info">
-                Information about Digital Marketing Website.
-              </p>
+              <h1 className="head">Spaceflight News App</h1>{' '}
+              <p className="info">Information about Spaceflight News App.</p>
               <a
                 className="got-app"
-                href="https://github.com/sarangsk01/Digital-Marketing-Website"
+                href="https://sarangsk01.github.io/TA-JS-asynchronous-js-TJaaao/block-BJaake/code/index.html"
+              >
+                Demo
+              </a>
+              <a
+                className="got-app"
+                href="https://github.com/sarangsk01/TA-JS-asynchronous-js-TJaaao/tree/block-BJaake/block-BJaake"
               >
                 Source
               </a>
             </li>
             <li className="pro-card">
-              <h1 className="head">Business Consulting Website</h1>{' '}
-              <p className="info">Information about Consulting Website.</p>
+              <h1 className="head">Image search app</h1>{' '}
+              <p className="info">Information about Image search app.</p>
               <a
                 className="got-app"
-                href="https://github.com/sarangsk01/Business-Consulting-Website"
+                href="https://sarangsk01.github.io/Image-search-app/code/index.html"
+              >
+                Demo
+              </a>
+              <a
+                className="got-app"
+                href="https://github.com/sarangsk01/Image-search-app"
               >
                 Source
               </a>
             </li>
             <li className="pro-card">
-              <h1 className="head">Website Making Company</h1>{' '}
-              <p className="info">Information about Website Making Company.</p>
+              <h1 className="head">Drag and Drop List</h1>{' '}
+              <p className="info">Information about Drag and Drop List.</p>
               <a
                 className="got-app"
-                href="https://github.com/sarangsk01/Website-Making-Company"
+                href="https://sarangsk01.github.io/TA-JS-advanced-dom-and-test-TJaaan/block-BJaait/code/index.html"
+              >
+                Demo
+              </a>
+              <a
+                className="got-app"
+                href="https://github.com/sarangsk01/TA-JS-advanced-dom-and-test-TJaaan/tree/block-BJaait/block-BJaait"
               >
                 Source
               </a>
